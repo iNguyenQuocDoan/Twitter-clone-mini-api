3d121f87498b9aec59c05dbe0eb1797ba7df6c43
iNguyenQuocDoan <quocdoanfb@gmail.com>
2025-08-22 09:02:23 +0700

    add comment

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,27 +1,28 @@
-import User from "~/model/schemas/User.shema"
-import databaseService from "./database.services"
+import User from '~/model/schemas/User.shema'
+import databaseService from './database.services'
 class UsersService {
-
-    async login(payload: { email: string, password: string }) {
-        const { email, password } = payload;
-
-        if (email === "nguyenquocdoan@gmail.com" && password === '123') {
+    async login(payload: { email: string; password: string }) {
+        const { email, password } = payload
+        if (email === 'nguyenquocdoan@gmail.com' && password === '123') {
             return {
-                message: "login success"
+                message: 'login success'
             }
         } else {
             return {
-                error: "login failed"
+                error: 'login failed'
             }
         }
     }
 
-    async register(payload: { email: string, password: string }) {
-        const { email, password } = payload;
-        const result = await databaseService.users.insertOne(new User({
-            email,
-            password
-        }))
+    async register(payload: { email: string; password: string }) {
+        const { email, password } = payload
+        // tao 1 user moi
+        const result = await databaseService.users.insertOne(
+            new User({
+                email,
+                password
+            })
+        )
 
         return result
     }
